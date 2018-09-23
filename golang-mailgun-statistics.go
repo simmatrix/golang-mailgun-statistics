1213143e3edff6ffb1596bff44ab1e106a8f989b
Vince Wong <laisim@netccentric.com>
2018-09-23 10:55:37 +0800

    Skipped those who doesn't have tags

--- golang-mailgun-statistics.go
+++ golang-mailgun-statistics.go
@@ -5,20 +5,19 @@ import (
 	"fmt"
 	"gopkg.in/mailgun/mailgun-go.v1"
 	"os"
-	"strings"
 )
 
 var privateKey = "key-xxxxx"
 var publicKey = "pubkey-xxxxx"
 
 func main() {
-	go process("lorem.com")
-	go process("ipsum.com")
+	go process("lorem.com", "lorem-newsletter")
+	go process("ipsum.com", "ipsum-newsletter")
 	var input string
 	fmt.Scanln(&input)
 }
 
-func process(domainName string) {
+func process(domainName string, keyword string) {
 	mg := mailgun.NewMailgun(domainName, privateKey, publicKey)
 	ei := mg.NewEventIterator()
 
@@ -69,29 +68,39 @@ func process(domainName string) {
 		var bufferTags bytes.Buffer
 
 		for _, log := range ei.Events() {
+			// Get the event that's related to this returned JSON data (e.g. opened, clicked, delivered etc.)
 			event := log["event"].(string)
-			replacer := strings.NewReplacer("[", "", "]", "", "*", "") // Replace "[" and "]" with an empty string
-			tags := replacer.Replace(fmt.Sprintf("%v", log["tags"]))   // convert the interface to string
 
-			// Only process those with campaign tags
-			if len(tags) > 0 {
+			// Check if the tag is relevant (has got to contain certain keyword)
+			var tags []interface{}
+			if log["tags"] != nil {
+				tags = log["tags"].(interface{}).([]interface{})
+			}
+
+			// Only process thosefmt with campaign tags, and match with the keyword specified
+			// We placed 2 Mailgun tags for each of the EDM that we send out
+			// the 1st one is unique to identify this EDM, whereas the 2nd one is to identify this email as an EDM
+			// Hence, over here we need to check that the 2nd tag is indeed equal to the keyword we provided on top
+			// As we don't wanna pull data that are related to other kind of emails being sent out (e.g. password reset email)
+			if len(tags) == 2 && tags[1] == keyword {
 
 				// Get the subject of the email
 				subject := "NULL" // So when we imported this CSV to MySQL later, the subject column would have NULL as its value
-				message := log["message"].(interface{}).(map[string]interface{})
-				for k1, v1 := range message {
-					if k1 == "headers" {
-						headers := v1.(interface{}).(map[string]interface{})
-						if headers["subject"] != nil {
-							subject = "\"" + headers["subject"].(string) + "\""
-							fmt.Println(subject)
+				if log["message"] != nil {
+					message := log["message"].(interface{}).(map[string]interface{})
+					for k1, v1 := range message {
+						if k1 == "headers" {
+							headers := v1.(interface{}).(map[string]interface{})
+							if headers["subject"] != nil {
+								subject = "\"" + headers["subject"].(string) + "\""
+							}
 						}
 					}
 				}
 
-				bufferTags.WriteString("\"" + tags + "\"," + subject)
+				bufferTags.WriteString("\"" + fmt.Sprintf("%v", tags[0]) + "\"," + subject)
 				bufferTags.WriteString("\n")
-				fmt.Println("[", event, "] (", tags, ")", subject)
+				fmt.Println("[", event, "] (", fmt.Sprintf("%v", tags[0]), ")", subject)
 
 				if event == "delivered" {
 					bufferDelivers.WriteString(log["recipient"].(string))
